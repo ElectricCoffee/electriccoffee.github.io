a2f293f777393d7468a08ef3411db77a7c75fcf9
ElectricCoffee <slench102+git@gmail.com>
2018-07-01 11:41:27 +0200

    Create initial version of Adventure

---
+++ adventure/src/main.js
@@ -0,0 +1,6 @@
+let textbox = document.getElementById("textbox");
+let page = document.body;
+
+textbox.innerHTML = "Testing 123";
+page.style.backgroundImage = "url(image.jpg)";
+page.style.backgroundSize = "100%";

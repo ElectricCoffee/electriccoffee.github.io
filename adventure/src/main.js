b4c89f0a1a7cd6b69859069c78c814d78d0e3033
Niko Lepka <slench102+git@gmail.com>
2018-07-02 10:43:37 +0200

    add event handlers to button and input

--- adventure/src/main.js
+++ adventure/src/main.js
@@ -4,6 +4,23 @@ const inputbox = document.getElementById("input-box");
 const goButton = document.getElementById("go-btn");
 const page = document.body;
 
+/** Event Hooks **/
+inputbox.addEventListener("keydown", onEnter);
+goButton.addEventListener("mousedown", readInput);
+
 textbox.innerHTML = "Testing 123";
 page.style.backgroundImage = "url(image.jpg)";
 page.style.backgroundSize = "100%";
+
+/** Event Handlers **/
+function onEnter(event) {
+    if (event.key === "Enter") {
+        readInput();
+    }
+}
+
+/** Other Functions **/
+function readInput() {
+    textbox.innerHTML = inputbox.value;
+    inputbox.value = "";
+}

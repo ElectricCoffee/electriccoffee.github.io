6e23e2a4e7bb18d13326f6a4fd9fe7b80ddd79b3
Niko Lepka <slench102+git@gmail.com>
2018-07-02 10:42:19 +0200

    make document objects consts

--- adventure/src/main.js
+++ adventure/src/main.js
@@ -1,5 +1,8 @@
-let textbox = document.getElementById("textbox");
-let page = document.body;
+/** DOM objects **/
+const textbox = document.getElementById("textbox");
+const inputbox = document.getElementById("input-box");
+const goButton = document.getElementById("go-btn");
+const page = document.body;
 
 textbox.innerHTML = "Testing 123";
 page.style.backgroundImage = "url(image.jpg)";
